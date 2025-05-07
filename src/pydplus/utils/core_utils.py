86b1444cf56adc7d74e930244a6ae920c086f9e8
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2025-05-06 18:20:31 -0600

    Added core_utils.py with header block

---
+++ src/pydplus/utils/core_utils.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+:Module:            pydplus.utils.core_utils
+:Synopsis:          Collection of supporting utilities and functions to complement the primary modules
+:Usage:             ``from pydplus.utils import core_utils``
+:Example:           ``encoded_string = core_utils.encode_url(decoded_string)``
+:Created By:        Jeff Shurtliff
+:Last Modified:     Jeff Shurtliff
+:Modified Date:     06 May 2025
+"""
+
