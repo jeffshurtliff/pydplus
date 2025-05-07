aeed779a2d2fe1cd7489f57e99d4033e8a675891
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2025-05-06 18:11:42 -0600

    Created core.py with header block

---
+++ src/pydplus/core.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+:Module:            pydplus.core
+:Synopsis:          This module performs the core operations of the package
+:Usage:             ``from pydplus import PyDPlus``
+:Example:           ``prod = PyDPlus()``
+:Created By:        Jeff Shurtliff
+:Last Modified:     Jeff Shurtliff
+:Modified Date:     06 May 2025
+"""
+
