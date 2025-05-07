412e31e650f9073eb4ec457277200279acfe4419
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2025-05-06 18:30:57 -0600

    Added pydplus.tests.resources module

---
+++ src/pydplus/tests/resources.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+:Module:            pydplus.utils.tests.resources
+:Synopsis:          Frequently used resources for performing unit testing
+:Usage:             ``from pydplus.utils.tests import resources``
+:Example:           TBD
+:Created By:        Jeff Shurtliff
+:Last Modified:     Jeff Shurtliff
+:Modified Date:     06 May 2025
+"""
+
+import os
+import sys
+import importlib
+
+import pytest
+
+# Define constants
+SKIP_LOCAL_TEST_MSG = 'skipping local-only tests'
+HELPER_FILE_NAME = 'helper_dev.yml'
+
+
+class MockResponse:
+    """This class simulates an API response for testing purposes.
+
+    .. versionadded:: 1.0.0
+    """
+    def __init__(self, json_body, status_code=200):
+        self.json_body = json_body
+        self.status_code = status_code
+
+    def json(self):
+        return self.json_body
+
+
+def mock_success_post(*args, **kwargs):
+    """This function works with the `MockedResponse` class to simulate a successful API response.
+
+    .. versionadded:: 1.0.0
+    """
+    # TODO: Update mock response below to mimic general Admin API success response
+    return MockResponse({
+        "id": "001D000000IqhSLIAZ",
+        "errors": [],
+        "success": True
+    })
+
+
+def mock_error_post(*args, **kwargs):
+    """This function works with the `MockedResponse` class to simulate a failed API response.
+
+    .. versionadded:: 1.0.0
+    """
+    # TODO: Update mock response below to mimic general Admin API error response
+    return MockResponse({
+        "errors": [],
+        "success": False
+    })
+
+
+def set_package_path():
+    """This function adds the high-level pydplus directory to the sys.path list.
+
+    .. versionadded:: 1.0.0
+    """
+    sys.path.insert(0, os.path.abspath('../..'))
+
+
+def import_modules(*modules):
+    """This function imports and returns one or more modules to utilize in a unit test.
+
+    .. versionadded:: 1.0.0
+
+    :param modules: One or more module paths (absolute) in string format
+    :returns: The imported module(s) as an individual object or a tuple of objects
+    """
+    imported_modules = []
+    for module in modules:
+        imported_modules.append(importlib.import_module(module))
+    tuple(imported_modules)
+    return imported_modules if len(imported_modules) > 1 else imported_modules[0]
+
+
+def secrets_helper_exists():
+    """This function checks to see if the unencrypted helper file exists for GitHub Actions.
+
+    .. versionadded:: 1.0.0
+    """
+    helper_path = f'{os.environ.get("HOME")}/secrets/{HELPER_FILE_NAME}'
+    return os.path.isfile(helper_path)
+
+
+def local_helper_exists():
+    """This function checks to see if a helper file is present in the ``local/`` directory.
+
+    .. versionadded:: 1.0.0
+    """
+    return os.path.exists(f'local/{HELPER_FILE_NAME}')
+
+
+# TODO: Add functions to instantiate core object with secrets or local helper
