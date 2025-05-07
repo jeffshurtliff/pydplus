b9d8b3112ff52691fa71a6a7ec00717ca1fbd2a2
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2025-05-07 13:51:50 -0600

    Created the auth.py module (empty)

---
+++ src/pydplus/auth.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+:Module:            pydplus.auth
+:Synopsis:          This module performs the authentication and authorization operations
+:Usage:             ``from pydplus import auth``
+:Example:           TBD
+:Created By:        Jeff Shurtliff
+:Last Modified:     Jeff Shurtliff
+:Modified Date:     07 May 2025
+"""
+
