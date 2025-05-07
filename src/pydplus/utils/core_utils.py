fe4473417ee42917da397b9314563bad1df4ea91
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2025-05-07 13:44:02 -0600

    Added imports and some functions to core_utils.py

--- src/pydplus/utils/core_utils.py
+++ src/pydplus/utils/core_utils.py
@@ -9,3 +9,42 @@
 :Modified Date:     06 May 2025
 """
 
+import random
+import string
+import os.path
+import warnings
+import urllib.parse
+
+import requests
+
+from . import log_utils
+from .. import errors
+
+# Initialize the logger for this module
+logger = log_utils.initialize_logging(__name__)
+
+
+def url_encode(raw_string):
+    """This function encodes a string for use in URLs.
+
+    .. versionadded:: 1.0.0
+
+    :param raw_string: The raw string to be encoded
+    :type raw_string: str
+    :returns: The encoded string
+    """
+    return urllib.parse.quote_plus(raw_string)
+
+
+def url_decode(encoded_string):
+    """This function decodes a url-encoded string.
+
+    .. versionadded:: 1.0.0
+
+    :param encoded_string: The url-encoded string
+    :type encoded_string: str
+    :returns: The unencoded string
+    """
+    return urllib.parse.unquote_plus(encoded_string)
+
+
