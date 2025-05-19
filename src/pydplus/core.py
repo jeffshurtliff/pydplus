86c1ae173e03c7a73e1e0a27db838cf0a6cd56de
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2025-05-18 18:44:06 -0600

    src/pydplus/utils/helper.py

--- src/pydplus/core.py
+++ src/pydplus/core.py
@@ -19,9 +19,11 @@ logger = log_utils.initialize_logging(__name__)
 class PyDPlus(object):
     """This is the class for the core object leveraged in this module."""
     # Define the function that initializes the object instance (i.e. instantiates the object)
-    def __init__(self, connection_type='oauth', private_key=None, helper=None):
+    def __init__(self, connection_info=None, connection_type='oauth', private_key=None, helper=None):
         """This method instantiates the core Salesforce object.
 
+        :param connection_info: Dictionary that defines the connection info to use
+        :type connection_info: dict, None
         :param connection_type: Defines the connection type to leverage ``oauth`` (default) or ``legacy``
         :type connection_type: str, None
         :param private_key: The file path to the private key used for API authentication
@@ -33,5 +35,22 @@ class PyDPlus(object):
         """
         # Define the default settings
         self._helper_settings = {}
+
+        # Check for provided connection info
+        if connection_info is None:
+            # Check for a supplied helper file
+            if helper:
+                # Parse the helper file contents
+                self.helper_path = helper
+                if any((isinstance(helper, tuple), isinstance(helper, list), isinstance(helper, set))):
+                    helper_file_path, helper_file_type = helper
+                elif isinstance(helper, str):
+                    helper_file_path, helper_file_type = (helper, 'json')
+                elif isinstance(helper, dict):
+                    helper_file_path, helper_file_type = helper.values()
+                else:
+                    error_msg = "The 'helper' argument can only be supplied as string, tuple, list, set or dict."
+                    logger.error(error_msg)
+                    raise TypeError(error_msg)
 
 
