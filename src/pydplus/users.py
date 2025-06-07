db2cd8a56b391464e2b4cf448ad089d28b3985d6
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2025-06-07 16:39:08 -0600

    Created skeleton of users.py module

---
+++ src/pydplus/users.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+:Module:            pydplus.users
+:Synopsis:          Defines the user-related functions associated with the RSA ID Plus API
+:Created By:        Jeff Shurtliff
+:Last Modified:     Jeff Shurtliff
+:Modified Date:     07 Jun 2025
+"""
+
+from . import errors
+from .utils import log_utils
+
+# Initialize logging
+logger = log_utils.initialize_logging(__name__)
+
+
